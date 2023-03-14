d6740c973dc87a8d1f416f25201a56bdf38dc487
patrick <pgousse@student.ubc.ca>
2023-03-14 16:51:40 -0400

    integrated hooks

--- frontend/src/components/ResultsTable.tsx
+++ frontend/src/components/ResultsTable.tsx
@@ -1,59 +1,49 @@
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { FC } from 'react';
-import { AlzheimersBarChart } from './BarChart';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { FC } from "react";
+import { AlzheimersBarChart } from "./BarChart";
 
-function createData(
-  gene: string,
-  location: number,
-  death: number,
-) {
-  return { gene, location, death};
+export interface ResultsProps {
+  data: {
+    apoe4genotype: string;
+    risk_factors: [string, number, string][];
+    risk_increase: number;
+  };
 }
 
-const rows = [
-  createData('AEO', 159, 6),
-  createData('EAAE', 237, 9),
-  createData('CCEECA', 262, 16),
-  createData('CEEEEAA', 305, 3),
-];
-
-export const ResultsTable: FC = () => {
-
-    return (
-        <TableContainer component={Paper}>
-          <Table sx={{ minWidth: 650 }} aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>Gene</TableCell>
-                <TableCell align="right">Location</TableCell>
-                <TableCell align="right">Death Rate&nbsp;</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {rows.map((row) => (
-                <TableRow
-                  key={row.gene}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {row.gene}
-                  </TableCell>
-                  <TableCell align="right">{row.location}</TableCell>
-                  {/* <TableCell align="right">{row.death}</TableCell> */}
-                  <TableCell align="right">
-                    <AlzheimersBarChart/>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
-      );
-}
+export const ResultsTable: FC<ResultsProps> = ({ data }) => {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Location</TableCell>
+            <TableCell>Genotype</TableCell>
+            <TableCell>Affect Size&nbsp;</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.risk_factors.map(([location, affectSize, genotype]) => (
+            <TableRow
+              key={genotype}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {location}
+              </TableCell>
+              <TableCell>{genotype}</TableCell>
+              <TableCell>
+                <AlzheimersBarChart affectSize={affectSize} />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,42 +1,41 @@
-import { Button, Stack } from "@mui/material"
+import { Button, Stack } from "@mui/material";
 import axios from "axios";
-import { FC, useState } from "react"
-import { ResultsTable } from "../components/ResultsTable";
+import { FC, useState } from "react";
+import { ResultsProps, ResultsTable } from "../components/ResultsTable";
 
 export const Home: FC = () => {
-
   const [selectedFile, setSelectedFile] = useState();
+  const [geneData, setGeneData] = useState<ResultsProps>({
+    data: {
+      apoe4genotype: "",
+      risk_factors: [["", 0, ""]],
+      risk_increase: 0,
+    },
+  });
 
-  const handleFileUpload = async (event:any) => {
+  const handleFileUpload = async (event: any) => {
     const file = event.target.files[0];
     setSelectedFile(file);
 
-    // Create a new FormData object
+    // send file to backend
     const formData = new FormData();
-
-    // Append the file to the form data
-    formData.append('file', file);
-    await axios.post(`http://127.0.0.1:5000/run`, formData)
-    .then((response) => {
-      // Handle the response here
-      console.log(response);
-    })
-    .catch((error) => {
-      // Handle errors here
-      console.error(error);
-    });
+    formData.append("file", file);
+    await axios
+      .post(`http://127.0.0.1:5000/run`, formData)
+      .then(async (response) => {
+        await setGeneData(response.data);
+        setGeneData((prevState) => ({
+          data: {
+            apoe4genotype: response.data.apoe4genotype,
+            risk_factors: response.data.risk_factors,
+            risk_increase: response.data.risk_increase,
+          },
+        }));
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   };
-
-    // axios.get(`http://127.0.0.1:5000/run?filename=${files[0].name}`)
-    // .then(function (response) {
-    //   console.log(response);
-    // })
-    // .catch(function (error) {
-    //   console.log(error);
-    // });
-    
-
-  
 
   return (
     <Stack direction="column" padding={4}>
@@ -47,10 +46,8 @@ export const Home: FC = () => {
         </Button>
       </Stack>
       <Stack direction="row">
-        <ResultsTable/>
+        {geneData && <ResultsTable data={geneData.data} />}
       </Stack>
-        
     </Stack>
-  
-  )
-}
+  );
+};
