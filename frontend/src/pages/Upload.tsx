61cde484ee7afdb3a83ae7d5ddb54d1adb926fe4
patrick <pgousse@student.ubc.ca>
2023-06-16 11:17:29 +0200

    add drag and drop functionality

--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -1,60 +1,86 @@
 import { Button, Stack, Typography } from "@mui/material";
 import axios from "axios";
 import { FC, useContext } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import { GeneContext } from "../context/geneContext";
 
 export const Upload: FC = () => {
-    const { state, updateState } = useContext(GeneContext)
-    const navigate = useNavigate()
+  const { state, updateState } = useContext(GeneContext);
+  const navigate = useNavigate();
 
-  const handleFileUpload = async (event: any) => {
-    // await axios
-    //   .post(`http://localhost:80/api/ping_api_as_post`) // http://127.0.0.1:80/api/analyze_genetics
-    //   .then(async (response) => {
-    //     console.log(response)
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //   });
-
-    const file = event.target.files[0];
-
-    // send file to backend
+  const handleFileUpload = async (file: File) => {
     const formData = new FormData();
     formData.append("file", file);
-    navigate('/overview')
-    await axios
-      .post(`http://localhost:80/api/analyze_genetics`, formData) // http://127.0.0.1:80/api/analyze_genetics
-      .then(async (response) => {
-        updateState({
-            apoe_genotype: response.data.apoe_genotype,
-            apoe_risk_factors: response.data.apoe_risk_factors,
-            apoe_risk_ratio: response.data.apoe_risk_ratio,
-            risk_factors: response.data.risk_factors,
-            risk_ratio: response.data.risk_ratio,
-          });
-      })
-      .catch((error) => {
-        console.error(error);
+    navigate("/overview");
+    try {
+      const response = await axios.post(
+        `http://localhost:80/api/analyze_genetics`,
+        formData
+      );
+      updateState({
+        apoe_genotype: response.data.apoe_genotype,
+        apoe_risk_factors: response.data.apoe_risk_factors,
+        apoe_risk_ratio: response.data.apoe_risk_ratio,
+        risk_factors: response.data.risk_factors,
+        risk_ratio: response.data.risk_ratio,
       });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file) {
+      handleFileUpload(file);
+    }
+  };
+
+  const handleDragOver = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      handleFileUpload(file);
+    }
   };
 
   return (
-    <Stack>
-      <Typography variant="h3">Upload your data</Typography>
-      <Typography variant="body1">
-        Drag and drop your .txt or .csv data file from 23andMe or other
-        providers like Ancestry DNA here.
-      </Typography>
-      <Button
-        variant="contained"
-        component="label"
-        sx={{ width: "fit-content" }}
+    <Stack sx={{ m: 4 }}>
+      <Typography variant="h1">Genetic Analyzer</Typography>
+      <Stack
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "80vh",
+        }}
+        onDrop={handleDrop}
+        onDragOver={handleDragOver}
       >
-        Browse
-        <input hidden multiple type="file" onChange={handleFileUpload} />
-      </Button>
+        <Typography variant="h1" sx={{ mb: 2 }}>
+          Upload your data
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{ mb: 4, width: 426, textAlign: "center" }}
+        >
+          Drag and drop your .txt or .csv data file from 23andMe or other
+          providers like Ancestry DNA here.
+        </Typography>
+        <Button
+          variant="contained"
+          component="label"
+          sx={{ width: "fit-content" }}
+        >
+          Browse
+          <input hidden multiple type="file" onChange={handleInputChange} />
+        </Button>
+      </Stack>
     </Stack>
   );
 };
